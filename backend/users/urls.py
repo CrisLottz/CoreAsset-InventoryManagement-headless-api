622f378d7b8380e167bf61cff588cd23dc50af57
Cris Lottz <cristophereugarrios@gmail.com>
2026-05-25 03:13:06 -0600

    sessionid active validation. Closes #14

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, UserMeView # Importamos la nueva vista
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='api-login'),
+    path('me/', UserMeView.as_view(), name='api-me'), # Nueva ruta
 ]
