6ba8e6ed8bc5258b9ae48498b30065401f47089f
Cris Lottz <cristophereugarrios@gmail.com>
2026-07-18 05:37:18 -0600

    Employees module. Closes #67

--- backend/employees/serializers.py
+++ backend/employees/serializers.py
@@ -4,4 +4,25 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ['id', 'first_name', 'last_name', 'job_title', 'is_active']
+        fields = [
+            'id', 
+            'employee_number', 
+            'first_name', 
+            'last_name', 
+            'job_title', 
+            'email', 
+            'is_active', 
+            'created_at', 
+            'updated_at'
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+    def validate_employee_number(self, value):
+        if value == "":
+            return None
+        return value
+
+    def validate_email(self, value):
+        if value == "":
+            return None
+        return value
